1ce4a924e3eb969dc2290d9c630ff04d9d29dc8a
Velprowq <1429463927@qq.com>
2026-06-07 21:46:12 +0800

    完成Lab13作业

--- 2025003008黄涛/Lab13/BookDto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bookshelf.model
-
-import com.google.gson.annotations.SerializedName
-
-data class BookDto(
-    val id: String = "",
-    @SerializedName("img_src")
-    val imgSrc: String = ""
-)
--- 2025003008黄涛/Lab13/BooksRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bookshelf.data
-
-import com.example.bookshelf.model.Book
-
-interface BooksRepository {
-    suspend fun getBooks(): List<Book>
-    suspend fun getBook(id: String): Book
-}
